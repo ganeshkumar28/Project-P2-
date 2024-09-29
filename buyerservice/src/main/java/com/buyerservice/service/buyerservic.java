1c911d55f891eab78009a547ac0c728cface026a
bhanu2586 <164385289+bhanu2586@users.noreply.github.com>
2024-09-29 10:33:46 +0530

    Add files via upload

---
+++ buyerservice/src/main/java/com/buyerservice/service/buyerservic.java
@@ -0,0 +1,100 @@
+package com.buyerservice.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.buyerservice.dao.buyerdaoservice;
+import com.buyerservice.dao.cartdaodervice;
+import com.buyerservice.dao.complaintdaoservice;
+import com.buyerservice.dao.orderdaointerface;
+import com.buyerservice.dao.productdaointerface;
+import com.buyerservice.dao.userdaointerface;
+import com.buyerservice.entity.Complaint;
+import com.buyerservice.entity.Orders;
+import com.buyerservice.entity.Products;
+import com.buyerservice.entity.Review;
+import com.buyerservice.entity.ShoppingCart;
+import com.buyerservice.entity.User;
+
+@Service
+@Transactional
+public class buyerservic implements buyerserviceinterface {
+	@Autowired
+	private buyerdaoservice bdao;
+	@Autowired
+	private complaintdaoservice ddao;
+	@Autowired
+	private cartdaodervice cdao;
+	@Autowired
+	private orderdaointerface odao;
+	@Autowired
+	private productdaointerface pdao;
+	@Autowired
+	private userdaointerface udao;
+	
+
+	@Override
+	public int submitreview(Review review) {
+		// TODO Auto-generated method stub
+		int i=0;
+		bdao.save(review);
+		i=1;
+		return i;
+	}
+
+	@Override
+	public int submitcomplaint(Complaint complaint) {
+		// TODO Auto-generated method stub
+		int i=0;
+		ddao.save(complaint);
+		return 0;
+	}
+
+	@Override
+	public List<ShoppingCart> getCartItemsByUserId(Long userId) {
+		// TODO Auto-generated method stub
+		
+		
+		return cdao.findByuserId(userId);
+	}
+
+	@Override
+	public int submitorder(Orders order) {
+		// TODO Auto-generated method stub
+		int i=0;
+		 odao.save(order);
+		 i=1;
+		 return i;
+	}
+
+	@Override
+	public Products getproductByUserId(Long pid) {
+		Optional<Products> pp=pdao.findById(pid);
+		return pp.get();
+	}
+
+	@Override
+	public User getByUserId(Long uid) {
+		Optional<User> pp=udao.findById(uid);
+		return pp.get();
+	}
+
+	@Override
+	public List<Review> getAllreviews(Long userId) {
+		// TODO Auto-generated method stub
+		
+		return bdao.findbyuserid(userId);
+	}
+
+	@Override
+	public void deleteReviewById(Long prid) {
+		// TODO Auto-generated method stub
+		bdao.deletebyproid(prid);
+		
+	}
+
+}
---
+++ buyerservice/src/main/java/com/buyerservice/service/buyerserviceinterface.java
@@ -0,0 +1,30 @@
+package com.buyerservice.service;
+
+import java.util.List;
+
+import com.buyerservice.entity.Complaint;
+import com.buyerservice.entity.Orders;
+import com.buyerservice.entity.Products;
+import com.buyerservice.entity.Review;
+import com.buyerservice.entity.ShoppingCart;
+import com.buyerservice.entity.User;
+
+public interface buyerserviceinterface {
+
+	int submitreview(Review review);
+
+	int submitcomplaint(Complaint complaint);
+
+	List<ShoppingCart> getCartItemsByUserId(Long userId);
+
+	int submitorder(Orders order);
+
+	Products getproductByUserId(Long pid);
+
+	User getByUserId(Long uid);
+
+	List<Review> getAllreviews(Long userId);
+
+	void deleteReviewById(Long prid);
+
+}
